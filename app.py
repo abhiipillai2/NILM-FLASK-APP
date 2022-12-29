e68e5007f513966c23a849949169ceda1ff5c126
abhijithpillai2 <abhijithpillai2@gmail.com>
2022-12-29 12:24:10 +0530

    second-commit

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ app.config['MYSQL_DB'] = 'nuclius_master'
 
 mysql = MySQL(app)
 
+#sequance generator function
 def sequesnceGenerator(arr,n):
     i=0
     arr1 = []
@@ -40,17 +41,18 @@ def sequesnceGenerator(arr,n):
     #arr1.append(temp)
     return arr1
 
-
+#json serialization class
 class NumpyArrayEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
+#main route
 @app.route("/mlAdapter" , methods = ['GET'])
 def mlAdapter():
 
-    #getting label max from data base
+    #getting n data from main db
     cursor = mysql.connection.cursor()
     cursor.execute(''' SELECT power,time_stamp from dcsub001 ''')
     #mysql.connection.commit()
@@ -61,36 +63,44 @@ def mlAdapter():
     for result in row:
         json_data.append(dict(zip(row_headers,result)))
     
-
+    #nessory arrays
     data_out_power= []
     data_out_time=[]
+
+    #formating data
     for i in range(0,len(json_data)):
         data_out_power.append(json_data[i]['power'])
         data_out_time.append(json_data[i]['time_stamp'])
 
+    #dataset list
     total_set ={"power":data_out_power,"time_stamp":data_out_time}
+    #data fareme making
     df = pd.DataFrame(data=total_set)
     df = df.set_index('time_stamp')
-    df.index = pd.to_datetime(df.index) 
+    df.index = pd.to_datetime(df.index)
+    #resampling 
     df = df.resample('10s').mean()
     df = df.dropna()
+    #seq value
     seq_val = 100
 
     X = df["power"]
     xx = np.array(X)
     X_TRAIN=np.array(sequesnceGenerator(xx,seq_val))
     
+    #model loading
     model_name = "NILM_BASE_MODEL_v_1_3.h5"
     loded_model = load_model(model_name)
 
+    #prediction
     predicted = loded_model(X_TRAIN)
     array = predicted.numpy()
     result = array.flatten()
     print(result)
 
-    numpyData = {"array": array}
+    #JSON Conversion
     encodedNumpyData = json.dumps(array, cls=NumpyArrayEncoder)
-    
+
     return jsonify({
     "statusCode": "200",
     "statusDesc": "Success",
